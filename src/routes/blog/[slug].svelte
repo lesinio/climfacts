6400e483eb17ea79b296292281edcf886248296b
Arnaud Oisel <arnaud.oisel@kosmos.fr>
2021-06-29 12:27:42 +0200

    test image 11

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -239,8 +239,9 @@
   <meta property="og:description" content={post.preview} />
   {#if post.preview_image}
     <meta property="og:image" content="{blogConfig.canon_host}{post.preview_image}" />
+    <!--
     <meta property="og:image:width" content="50">
-    <meta property="og:image:height" content="50">
+    <meta property="og:image:height" content="50">-->
   {/if}
 
   <!-- Twitter -->
