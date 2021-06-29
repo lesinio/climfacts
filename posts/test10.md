20a2a05be4fb5a29b56dfec12222388e6b843ffc
Arnaud Oisel <arnaud.oisel@kosmos.fr>
2021-06-29 12:17:54 +0200

    test image 10

---
+++ posts/test10.md
@@ -0,0 +1,9 @@
+---
+title: test 10 title
+date: 2020-10-09
+author: Lesinio
+preview: "test 10 preview"
+preview_image: /posts/test/Example.jpg
+---
+
+<p>test6</p>
--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -239,8 +239,8 @@
   <meta property="og:description" content={post.preview} />
   {#if post.preview_image}
     <meta property="og:image" content="{blogConfig.canon_host}{post.preview_image}" />
-    <meta property="og:image:width" content="1080">
-    <meta property="og:image:height" content="1080">
+    <meta property="og:image:width" content="50">
+    <meta property="og:image:height" content="50">
   {/if}
 
   <!-- Twitter -->
